0fb4b279c4525d870e1af6cd89f8d84007f012e3
Drew <andrew.christofferson97@gmail.com>
2021-04-20 21:03:24 -0600

    added a couple tests

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -4,14 +4,13 @@ import { Redirect, useHistory } from 'react-router-dom';
 import { Formik, Field, Form, FormikHelpers } from "formik";
 import axios from 'axios';
 import AppContext from '../../context/context';
-import { Link } from 'react-router-dom'; 
+import { BrowserRouter as Router, Link } from 'react-router-dom'; 
 import { EntryPage, PageHeader } from '../../Components/Login/login'; 
 import EntryCard from '../../Components/EntryCard/EntryCard';
 import InputGroup from '../../Components/InputGroup/InputGroup';
 import LoginInput from '../../Components/Input/Input';
 import LoginButton from '../../Components/LoginButton/LoginButton';
-import { IoMdBookmarks } from 'react-icons/io';
-
+import Promo from './loginpromo'
 
 
 const Container = styled.div`
@@ -27,47 +26,6 @@ const FormContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-`
-
-const PromoContainer = styled.div`
-    flex-basis: 40%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: black;
-    color: white;
-    padding-bottom: 10%;
-`
-
-const Title = styled.h1`
-    font-size: 50px;
-`
-const QuoteRight = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    width: 90%;
-    font-size: 68px;
-    font-weight: 800;
-`
-const QuoteLeft = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    width: 90%;
-    font-size: 68px;
-    font-weight: 800;
-`
-const QuoteContent = styled.div`
-    width: 75%;
-    font-size: 24px;
-    font-weidht: 400;
-    font-style: italic; 
-`
-const QuoteAuthorContent = styled.div`
-    width: 50%;
-    font-size: 20px;
-    font-weidht: 500;
 `
 
 interface MyFormValues {
@@ -111,15 +69,9 @@ export default function Login() {
     }
 
     return (
+        <Router>
         <Container>
-            <PromoContainer>
-                <IoMdBookmarks size="100"/>
-                <Title>DevJournal</Title>
-                <QuoteLeft>&#8220;</QuoteLeft>
-                <QuoteContent>This journal app for developers will make all your wildest dreams come true.</QuoteContent>
-                <QuoteRight>&#8221;</QuoteRight>
-                <QuoteAuthorContent>- John Turner, Super Smart Developer</QuoteAuthorContent>
-            </PromoContainer>
+            <Promo />
             <FormContainer>
             <EntryPage>
                 {/* <PageHeader to="/">Awesome Journal</PageHeader> */}
@@ -156,5 +108,6 @@ export default function Login() {
             </EntryPage>
           </FormContainer>
         </Container>
+        </Router>
     );
 }
